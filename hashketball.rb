4056d0777b22a4303d69aa16fc50ce7c04e35fa1
Christina Chianelli <cechianelli@gmail.com>
2020-05-29 14:53:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,14 +4,14 @@ game_hash = {
     :team_name => "Brooklyn Nets",
     :colors => ["Black", "White"],
     :players => [
-      {:player_name => "",
-      :number => "",
-      :shoe =>"",
-      :points => "",
-      :rebounds => "",
-      :assists => "",
-      :steals => "",
-      :blocks => "",
+      {:player_name => "Alan Anderson",
+      :number => "0",
+      :shoe =>"16",
+      :points => "22",
+      :rebounds => "12",
+      :assists => "12",
+      :steals => "3",
+      :blocks => "1",
       :slam_dunks => ""}, 
       
       
